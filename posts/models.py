0ef3a173c26210ae44a28e8f7b321b5483b3f3af
iamIkay <jdon144@gmail.com>
2023-08-16 16:10:09 +0100

    refactor: changed datetime format in Post model

--- posts/models.py
+++ posts/models.py
@@ -1,7 +1,8 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
 class Post(models.Model):
     title= models.CharField(max_length= 50)
     content= models.CharField(max_length= 1000)
-    date= models.CharField(max_length=30)
+    date= models.DateTimeField(default=datetime.now, blank=True)
